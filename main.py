02283a736d67b5cc24fc4521ab03c258668e4853
Lloyd Lobo <lloydlobo4@gmail.com>
2023-11-03 20:08:10 +0530

    feat: enable filter-rich repoitory selection
    Refactors hardcoded values to global configurable constants.
    Also uses better variable names.
    Adds comments as needed.
    Better documented usage at top level doc comments.
    Uses generator to filter out ignored repos(archived, forks, etc.).
    Adds new fields to sort output repo list.

--- main.py
+++ main.py
@@ -1,103 +1,161 @@
-"""
-gitdash/main.py
+# -*- coding: utf-8 -*-
 
-# Pre-requisites
+# gitdash/main.py
 
-This workflow requires two environment variables: `GH_USERNAME` and `GH_TOKEN`.
-They need to be added as Action secrets in 'Security/Secrets and variables/Actions'.
+# `gitdash` generates pretty Markdown lists of git repositories.
+#
+# # Pre-requisites
+# 
+# This workflow requires two environment variables: `GH_USERNAME` and
+# `GH_TOKEN`. They need to be added as Action secrets in 'Security/Secrets
+# and variables/Actions'.
+# 
+# * `GH_USERNAME` is your GitHub account's username.
+# * `GH_TOKEN` is a # personal access token generated from the GitHub website.
+# 
+# <GH_TOKEN> Personal Access Token(PAT): Minimum Requirements:
+# 
+# * repo
+# * admin:org -> read:org (optional)
+# * admin:public_key -> read.public_key (optional)
+# * admin:repo_hook -> read:repo_hook (optional)
+# * notification (optional)
+# * audit_log -> read:audit_log (optional)
+# 
+# To generate it, follow these steps:
+# 
+# * Log in to your GitHub account and go to your settings.
+# * In the left-side menu, click on Developer Settings.
+# * Click on Personal Access Tokens.
+# * Click on Generate new token.
+# * Provide a name for your token and select the scopes that you need.
+# * Click on Generate token.
+# 
+# You will only see the token once. Make sure to save it in a safe place.
+# [See also]
+# (https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-authentication-to-github)
 
-* `GH_USERNAME` is your GitHub account's username.
-* `GH_TOKEN` is a personal access token that can be generated from the GitHub website.
-
-<GH_TOKEN>: minimum requirements: 
-
-* `admin:org` 
-* `admin:public_key`
-* `repo`.
-
-To generate it, follow these steps:
-
-* Log in to your GitHub account and go to your settings.
-* In the left-side menu, click on Developer Settings.
-* Click on Personal Access Tokens.
-* Click on Generate new token.
-* Provide a name for your token and select the scopes that you need.
-* Click on Generate token.
-
-You will only be able to see the token once. Make sure to save it in a safe place.
-
-[See also]
-(https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-authentication-to-github)
-"""
-
-import os
+from os import getenv
+from sys import exit, stderr
 from typing import Any, Iterable
 
-import requests
-from dotenv import load_dotenv  # `python-dotenv`
-from requests import Response
+from dotenv import load_dotenv  # `python-dotenv`.
+from requests import get, Response
+
+# ——————————————————————————————————————————————————————————————————————————
+# Configuration Constants.
+REPO_VISIBILITY = "public"
+REPO_AFFILIATION = "owner"
+REPO_SORT = "created"
+REPO_PER_PAGE = 100  # min: 30, max: 100. Repos to fetch at each GET request.
+REPO_IGNORED_KEYS = {"archived", "fork", }  # Must have a `bool` # API value.
+
+# ——————————————————————————————————————————————————————————————————————————
+# Desired Repository Property Constants(to emit to markdown.)
+OUT_REPO_PARAMS = ["name", "language", "size", "license", "description", ]
+OUT_MAX_COL_LEN = 80  # Desired Markdown item length(clamp long descriptions.)
+
+# ——————————————————————————————————————————————————————————————————————————
+# CI GitHub Workflow Action specific Constants.
+GITDASH_OUTFILE_MD = "out.md"  # Output file path to emit markdown to.
+GITDASH_ENV_GH_USERNAME = "GH_USERNAME"  # GitHub account username in `.env`.
+GITDASH_ENV_GH_TOKEN = "GH_TOKEN"  # GitHub Personal Access Token in `.env`.
+
+# ——————————————————————————————————————————————————————————————————————————
+# GitHub API Constants.
+API_BASE_URL = "https://api.github.com"
+API_HEADER_VERSION = "2022-11-28"
+API_HEADER_ACCEPT = "application/vnd.github+json"
+API_STATUS_UNKNOWN_MSG = "Unknown status code"
+API_STATUS_OK = 200
+API_STATUS_CODES = {
+    200: "OK",
+    304: "Not modified",
+    401: "Forbidden",
+    422: "Validation failed, or the endpoint has been spammed.",
+}
 
 
-def fmt_repo_field(repository: Any, field: str) -> str:
-    max_str_len = 80  # Desired Markdown list item length(to clamp long descriptions.)
-    repo_val = repository[field]
-
-    match field:
+# Format a repository property based on the provided key.
+def fmt_repo(repo: Any, key: str) -> str:
+    prop: object = repo[key]
+    match key:
         case "name":
-            return f"[{repo_val}]({repository['html_url']})"
+            return f"[{prop}]({repo['html_url']})"
         case "language", "size":
-            return f"`{repo_val}`"
-        case "license" if repo_val:
-            return f"`{repo_val['spdx_id']}`"
-        case _ if ((repo_val is not None) and (len(f"{repo_val}") > max_str_len)):
-            return f"`{repo_val[:max_str_len]}...`"
+            return f"`{prop}`"
+        case "license" if prop:
+            return f"`{prop['spdx_id']}`"
+        case _ if (prop is not None and
+                   len(f"{prop}") > OUT_MAX_COL_LEN):
+            return f"`{prop[:OUT_MAX_COL_LEN]}...`"
         case _:
-            return f"`{repo_val}`"
+            return f"`{prop}`"
 
 
-def main() -> None:
-    load_dotenv()
+# Main function fetches user's GitHub repositories,
+# and parses it as a pretty list and writes to a Markdown file.
+def run() -> None:
+    load_dotenv()  # Load environment variables.
 
-    gh_username: str = os.getenv("GH_USERNAME")
-    gh_token: str = os.getenv("GH_TOKEN")
-    gh_repo_fields: list[str] = ["name", "language", "size", "license", "description"]
+    # ——————————————————————————————————————————————————————————————————————————
+    # Fetch and parse user's credentials stored as environment variables.
+    gh_username: str = getenv(GITDASH_ENV_GH_USERNAME)
+    gh_token: str = getenv(GITDASH_ENV_GH_TOKEN)
+    if not (gh_token or gh_username):
+        print("error: Neither GH_TOKEN nor GH_USERNAME is provided. "
+              "Please set at least one environment variable.", file=stderr, )
+        exit(1)
 
-    api_url = f"https://api.github.com/users/{gh_username}/repos"
     api_headers: dict[str, str] = {
-        "X-GitHub-Api-Version": "2022-11-28",
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"token {gh_token}",
+        "X-GitHub-Api-Version": API_HEADER_VERSION,
+        "Accept":               API_HEADER_ACCEPT,
     }
+    if gh_token:
+        api_headers["Authorization"] = f"Bearer {gh_token}"
 
-    page = 1
+    api_url: str = (f"{API_BASE_URL}"
+                    f"/{'user' if gh_token else f'users/{gh_username}'}"
+                    f"/repos")
+
+    # ——————————————————————————————————————————————————————————————————————————
+    # Query and collect user's repositories via GET request.
     all_repos: list[Any] = []
+    params: dict[str, int | str] = dict(
+        page=1, per_page=REPO_PER_PAGE, sort=REPO_SORT,
+        visibility=REPO_VISIBILITY, affiliation=REPO_AFFILIATION)
 
     while True:
-        params: dict[str, int] = {"page": page, "per_page": 100}
-
-        response: Response = requests.get(url=api_url, params=params, headers=api_headers)
-
-        if response.status_code == 200:
-            gh_repos: Iterable[Any] = response.json()
-            if not gh_repos:
-                break
-
-            page += 1
-            all_repos.extend(gh_repos)
-        else:
-            print("failed to retrieve repositories. Status code:", response.status_code)  # 401
+        resp: Response = get(url=api_url, params=params,
+                             headers=api_headers)
+        status = resp.status_code
+        if not status == API_STATUS_OK:
+            err = API_STATUS_CODES.get(status, __default=API_STATUS_UNKNOWN_MSG)
+            print(f"error:({status}): GET request failed: {err}", file=stderr, )
             break
 
-    out_buffer: list[str] = [f"__{','.join(gh_repo_fields)}__\n"]
+        gh_repos: Iterable[Any] = resp.json()
+        if not gh_repos:
+            break
 
-    for repo in all_repos:
-        if not repo["archived"]:
-            item: list[str] = [fmt_repo_field(repository=repo, field=field) for field in gh_repo_fields]
-            out_buffer.append(f"* {','.join(item)}\n")
+        all_repos.extend(gh_repos)
+        params['page'] += 1
 
-    with open("out.md", "w", encoding="utf-8") as out_file:
+    # ——————————————————————————————————————————————————————————————————————————
+    # Prepare pretty formatted Markdown to write to output file.
+    out_buffer: list[str] = list(f"__{','.join(OUT_REPO_PARAMS)}__\n")
+    gen_repos = (r for r in all_repos
+                 if not any(r[key] for key in REPO_IGNORED_KEYS))
+
+    for repo in gen_repos:
+        item = [fmt_repo(repo=repo, key=key) for key in OUT_REPO_PARAMS]
+        out_buffer.append(f"* {','.join(item)}\n")
+
+    # ——————————————————————————————————————————————————————————————————————————
+    # Write the generated Markdown buffer to output file.
+    with open(GITDASH_OUTFILE_MD, "w", encoding="utf-8") as out_file:
         out_file.writelines(out_buffer)
 
 
 if __name__ == '__main__':
-    main()
+    exit(run())
